23f442d03f04d8058e057de4b708edb41724b322
Myckol Fontes <myckol.ti@gmail.com>
2022-06-02 14:49:31 -0300

    criando rota de busca

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProductController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,6 +20,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('products', function(){
-    return \App\Models\Product::all();
-});
+// Route::get('products', function(){
+//     return \App\Model\Product::all();
+// });
+
+Route::get('products', [ProductController::class, 'index']);
