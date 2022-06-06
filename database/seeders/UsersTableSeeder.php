eeed1f66d8884be9c1218992fa72fa8685fb3287
Myckol Fontes <myckol.ti@gmail.com>
2022-06-06 11:59:50 -0300

    criando usuário aleatório com seeder

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // factory(\App\Models\User::class, 1)->create();
+        \App\Models\User::factory()->count(1)->create();
+    }
+}
